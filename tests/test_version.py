1896e2dce9a1dd2e07856517f50c41497d9f1133
Tom de Geus <tom@geus.me>
2022-08-22 17:24:27 +0200

    Removing test on version

--- tests/test_version.py
+++ tests/test_version.py
@@ -7,10 +7,6 @@ import GMatElastoPlastic as GMat
 class Test_main(unittest.TestCase):
     """ """
 
-    def test_version(self):
-
-        self.assertEqual(setuptools_scm.get_version(), GMat.version())
-
     def test_version_dependencies(self):
 
         deps = GMat.version_dependencies()
